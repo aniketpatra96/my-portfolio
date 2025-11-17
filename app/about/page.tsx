bb2ae604af7b722d9053088e94eb3b953f433e3c
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-11-17 20:12:06 +0530

    completed about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -179,6 +179,38 @@ export default function AboutPage(): ReactElement {
                   </div>
                 </div>
               </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Php developer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-2 text-sm italic">
+                    My best experience so far!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2021 - 2022
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Syllogistek Systems
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3"></div>
+              </div>
             </div>
           </div>
         </div>
